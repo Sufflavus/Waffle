3b4e02b1a8e185ff15c446b72ca3fd0d29163795
Sufflavus <tatashat@gmail.com>
2015-02-19 22:57:05 +0300

    + WCF rest service for sending and getting messages

---
+++ Source/Bijuu.Service/IRequestProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+
+namespace Bijuu.Service
+{
+    [ServiceContract]
+    public interface IRequestProcessor
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetAllMessages?message={message}")]
+        void GetAllMessages(string message);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetNewMessages?userId={userId}")]
+        void GetNewMessages(Guid userId);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/LogIn?userName={userName}")]
+        Guid LogIn(string userName);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/LogOut?userId={userId}")]
+        void LogOut(Guid userId);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/SendMessage?message={message}")]
+        int SendMessage(string message);
+    }
+}
---
+++ Source/Bijuu.Service/RequestProcessor.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Bijuu.Service
+{
+    public class RequestProcessor : IRequestProcessor
+    {
+        public void GetAllMessages(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void GetNewMessages(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Guid LogIn(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void LogOut(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public int SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
