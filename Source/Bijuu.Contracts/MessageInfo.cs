86d2cef26838db8dc75c7010df80d426df1b4fdc
Sufflavus <tatashat@gmail.com>
2015-02-21 21:17:47 +0300

    + Contracts for service

---
+++ Source/Bijuu.Contracts/MessageInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Bijuu.Contracts
+{
+    [DataContract]
+    public class MessageInfo
+    {
+        [DataMember(IsRequired = false)]
+        public DateTime? CreateDate { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public bool IsDelivered { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public Guid RecipientId { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public Guid SenderId { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Text { get; set; }
+    }
+}
---
+++ Source/Bijuu.Contracts/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Bijuu.Contracts
+{
+    [DataContract]
+    public sealed class UserInfo
+    {
+        [DataMember(IsRequired = true)]
+        public bool IsOnline { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Name { get; set; }
+    }
+}
