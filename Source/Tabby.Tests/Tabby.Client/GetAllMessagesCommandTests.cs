2471cd7d171a8ff6ae2c2a70929bd082a221986a
Sufflavus <tatashat@gmail.com>
2015-03-13 23:36:17 +0300

    + tests for MessageCommand

---
+++ Source/Tabby.Tests/Tabby.Client/GetAllMessagesCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NSubstitute;
+
+using Tabby.Client.Command.MessageModule;
+using Tabby.Client.Logger;
+
+using Taddy.BusinessLogic.Models;
+using Taddy.BusinessLogic.Processor;
+
+using Xunit;
+
+
+namespace Waffle.Tests.Tabby.Client
+{
+    public class GetAllMessagesCommandTests
+    {
+        [Fact]
+        public void Execute_MessageProcessor_IsCalled()
+        {
+            var messageProcessor = Substitute.For<IMessageProcessor>();
+            messageProcessor.GetAllMessages().Returns(new List<Message>());
+            var logger = Substitute.For<ILogger>();
+            var command = new GetAllMessagesCommand { MessageProcessor = messageProcessor, Logger = logger };
+
+            command.Execute();
+
+            messageProcessor.Received().GetAllMessages();
+        }
+    }
+}
