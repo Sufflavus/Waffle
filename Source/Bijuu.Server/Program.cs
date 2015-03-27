c4d43dd0f093d71c9c7530821898fea94341a6bd
Sufflavus <tatashat@gmail.com>
2015-03-27 23:19:20 +0300

    * Server initialization

--- Source/Bijuu.Server/Program.cs
+++ Source/Bijuu.Server/Program.cs
@@ -10,12 +10,14 @@ namespace Bijuu.Server
     {
         private static void Main(string[] args)
         {
-            var host = new WebServiceHost(typeof(RequestProcessor));
-            host.Open();
-            Console.WriteLine("Service is running");
-            Console.WriteLine("Press enter to quit...");
-            Console.ReadLine();
-            host.Close();
+            using (var host = new WebServiceHost(typeof(RequestProcessor)))
+            {
+                host.Open();
+                Console.WriteLine("Service is running");
+                Console.WriteLine("Press enter to quit...");
+                Console.ReadLine();
+                host.Close();
+            }
         }
     }
 }
