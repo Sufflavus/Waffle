8d8b2ae2d6675d9a5ca34ab80f74f21a8e6e6010
Sufflavus <tatashat@gmail.com>
2015-05-10 23:20:08 +0300

    + NotificationReceiverWrapper

---
+++ Source/Tabby.Terminal/NotificationReceiverWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Ginger.Notifier;
+
+using Microsoft.Practices.Unity;
+
+using Taddy.BusinessLogic.Models;
+
+
+namespace Tabby.Terminal
+{
+    public sealed class NotificationReceiverWrapper
+    {
+        [Dependency]
+        public INotificationReceiver NotificationReceiver { get; set; }
+
+
+        public void Init(Action<Message> onMessageReceive, Action<User> onUserStateChanged)
+        {
+            NotificationReceiver.SubscribeForReceivingMessage(x => onMessageReceive(NotifierConverter.ToMessage(x)));
+            NotificationReceiver.SubscribeForReceivingUserState(x => onUserStateChanged(NotifierConverter.ToUser(x)));
+        }
+    }
+}
