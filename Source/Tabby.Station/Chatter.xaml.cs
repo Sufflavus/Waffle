923aa8b02909a58ae32d5dd6c8dabd6a331b219a
Sufflavus <tatashat@gmail.com>
2015-06-23 23:32:41 +0300

    * MainWindow.xaml -> Chatter.xaml * Chatter window: Loaded event handler

---
+++ Source/Tabby.Station/Chatter.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+
+namespace Tabby.Station
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Chatter : Window
+    {
+        public Chatter()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Chatter_Loaded(object sender, RoutedEventArgs e)
+        {
+            TbRecentMessages.Text = "Hello!";
+        }
+    }
+}
