d1704f2e021ab7ec517526517760f46f1add1ee7
Sufflavus <tatashat@gmail.com>
2015-06-28 16:22:35 +0300

    * Tabby.Station Bootstrapper has been added

---
+++ Source/Tabby.Station/Bootstrapper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Bijuu.ServiceProvider;
+
+using Ginger.Notifier;
+
+using Microsoft.Practices.Unity;
+
+using Taddy.BusinessLogic;
+using Taddy.BusinessLogic.Processor;
+
+
+namespace Tabby.Station
+{
+    internal sealed class Bootstrapper : IDisposable
+    {
+        static Bootstrapper()
+        {
+            CreateContainer();
+        }
+
+
+        private static IUnityContainer Container { get; set; }
+
+        public static T Resolve<T>(params ParameterOverride[] overrides)
+        {
+            return Container.Resolve<T>(overrides);
+        }
+
+
+        public static void Dispose()
+        {
+            Container.Dispose();
+        }
+
+
+        private static void CreateContainer()
+        {
+            Container = new UnityContainer();
+
+            Container.RegisterType<IMessageProcessor, MessageProcessor>();
+            Container.RegisterType<IUserProcessor, UserProcessor>();
+            Container.RegisterType<IBijuuServiceClient, BijuuServiceClient>();
+
+            Container.RegisterType<INotificationReceiver, NotificationReceiver>();
+
+            Container.RegisterType<NotificationReceiverWrapper>();
+        }
+
+
+        void IDisposable.Dispose()
+        {
+            Dispose();
+        }
+    }
+}
--- Source/Tabby.Station/Chatter.xaml.cs
+++ Source/Tabby.Station/Chatter.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Windows;
 
+using Microsoft.Practices.Unity;
+
+using Taddy.BusinessLogic.Models;
+using Taddy.BusinessLogic.Processor;
+using Taddy.BusinessLogic.Converters;
+
 
 namespace Tabby.Station
 {
@@ -9,6 +15,11 @@ namespace Tabby.Station
     /// </summary>
     public partial class Chatter : Window
     {
+        private Guid _userId;
+
+        [Dependency]
+        public IMessageProcessor MessageProcessor { get; set; }
+
         public Chatter()
         {
             InitializeComponent();
@@ -18,6 +29,14 @@ namespace Tabby.Station
         private void Chatter_Loaded(object sender, RoutedEventArgs e)
         {
             TbRecentMessages.Text = "Hello!";
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            string messageText = TbMessageText.Text;
+            Message message = BusinessLogicConverter.ToMessage(messageText);
+            message.SenderId = _userId;
+            int result = MessageProcessor.SendMessage(message);
         }
     }
 }
